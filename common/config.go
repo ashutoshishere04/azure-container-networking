3ada9ca3f4cf3c7f3092a265955e375fcf011519
vivekagg-MSFT <36711748+ninzavivek@users.noreply.github.com>
2019-12-05 14:01:00 -0800

    Store file override (#452)

--- common/config.go
+++ common/config.go
@@ -88,4 +88,8 @@ const (
 	// HTTP response header timeout
 	OptHttpResponseHeaderTimeout      = "http-response-header-timeout"
 	OptHttpResponseHeaderTimeoutAlias = "httprespheadertimeout"
+
+	// Store file location
+	OptStoreFileLocation      = "store-file-path"
+	OptStoreFileLocationAlias = "storefilepath"
 )
